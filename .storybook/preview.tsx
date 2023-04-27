2466d27ccd41811454c2103251149b3a1555426a
asdf99245 <asdf99245@naver.com>
2023-04-28 00:53:59 +0900

    feat: storybook 전역 StyleProvider 적용

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,4 +1,6 @@
+import React from 'react';
 import type { Preview } from '@storybook/react';
+import StyleProvider from '../src/styles/StyleProvider';
 
 const preview: Preview = {
   parameters: {
@@ -10,6 +12,13 @@ const preview: Preview = {
       },
     },
   },
+  decorators: [
+    (Story) => (
+      <StyleProvider>
+        <Story />
+      </StyleProvider>
+    ),
+  ],
 };
 
 export default preview;
