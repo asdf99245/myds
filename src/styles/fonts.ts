dbd811f48fce8b704210f4531bdef6bba5fbaa68
asdf99245 <asdf99245@naver.com>
2023-04-28 13:36:38 +0900

    refactor: font-family, font-weight 프로퍼티명 변경

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,6 +1,6 @@
 interface Font {
-  fontFamily: string;
-  fontWeight: number;
+  family: string;
+  weight: number;
   size: string;
   lineHeight: string;
   letterSpacing: string;
@@ -21,78 +21,78 @@ export type FontVariant =
 
 export const FONTS: Record<FontVariant, Font> = {
   display: {
-    fontFamily: 'Pretendard',
-    fontWeight: 700,
+    family: 'Pretendard',
+    weight: 700,
     size: '50px',
     lineHeight: '65px',
     letterSpacing: '0px',
   },
   heading1: {
-    fontFamily: 'Pretendard',
-    fontWeight: 700,
+    family: 'Pretendard',
+    weight: 700,
     size: '40px',
     lineHeight: '52px',
     letterSpacing: '0px',
   },
   heading2: {
-    fontFamily: 'Pretendard',
-    fontWeight: 700,
+    family: 'Pretendard',
+    weight: 700,
     size: '36px',
     lineHeight: '47px',
     letterSpacing: '0px',
   },
   heading3: {
-    fontFamily: 'Pretendard',
-    fontWeight: 700,
+    family: 'Pretendard',
+    weight: 700,
     size: '32px',
     lineHeight: '42px',
     letterSpacing: '0px',
   },
   heading4: {
-    fontFamily: 'Pretendard',
-    fontWeight: 700,
+    family: 'Pretendard',
+    weight: 700,
     size: '28px',
     lineHeight: '36px',
     letterSpacing: '0px',
   },
   heading5: {
-    fontFamily: 'Pretendard',
-    fontWeight: 700,
+    family: 'Pretendard',
+    weight: 700,
     size: '24px',
     lineHeight: '31px',
     letterSpacing: '0px',
   },
   title: {
-    fontFamily: 'Pretendard',
-    fontWeight: 700,
+    family: 'Pretendard',
+    weight: 700,
     size: '20px',
     lineHeight: '30px',
     letterSpacing: '0px',
   },
   subTitle: {
-    fontFamily: 'Pretendard',
-    fontWeight: 700,
+    family: 'Pretendard',
+    weight: 700,
     size: '18px',
     lineHeight: '27px',
     letterSpacing: '0px',
   },
   body1: {
-    fontFamily: 'Pretendard',
-    fontWeight: 400,
+    family: 'Pretendard',
+    weight: 400,
     size: '16px',
     lineHeight: '24px',
     letterSpacing: '0px',
   },
   body2: {
-    fontFamily: 'Pretendard',
-    fontWeight: 400,
+    family: 'Pretendard',
+    weight: 400,
     size: '14px',
     lineHeight: '21px',
     letterSpacing: '0px',
   },
   caption: {
-    fontFamily: 'Pretendard',
-    fontWeight: 400,
+    family: 'Pretendard',
+    weight: 400,
     size: '12px',
     lineHeight: '18px',
     letterSpacing: '0px',
