9c1fbbfb0b0913d1d3594b955d47dcb076098695
asdf99245 <asdf99245@naver.com>
2023-05-03 15:44:07 +0900

    feat: Icon 컴포넌트 구현

---
+++ src/components/Icon/Icon.tsx
@@ -0,0 +1,18 @@
+import * as icons from 'src/assets/icons';
+
+type IconType = keyof typeof icons;
+
+interface Props {
+  icon: IconType;
+  color?: string;
+  size?: string | number;
+}
+
+function Icon({ icon, color, size }: Props) {
+  /*eslint import/namespace: ['error', { allowComputed: true }]*/
+  const SVGIcon = icons[icon];
+
+  return <SVGIcon css={{ fill: color || 'currentColor', width: size, height: 'auto' }} />;
+}
+
+export default Icon;
