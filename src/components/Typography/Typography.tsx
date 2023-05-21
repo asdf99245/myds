40dd3fa5f11c5879b03b3e1246f76a8a68f1bb40
asdf99245 <asdf99245@naver.com>
2023-05-21 20:10:45 +0900

    refactor: Typography 다형성 컴포넌트 유틸 타입 적용

--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -1,32 +1,27 @@
-import { CSSProperties, ComponentPropsWithoutRef, ElementType, ReactNode } from 'react';
+import { CSSProperties, ElementType } from 'react';
 import { useTheme } from '@emotion/react';
 import { FontVariant } from '@styles/fonts';
+import { PolymorphicProps } from '@utils/type.util';
 
-interface Props extends ComponentPropsWithoutRef<'span'> {
-  children: ReactNode;
+interface TypographyProps {
   variant?: FontVariant;
   fontSize?: CSSProperties['fontSize'];
   fontWeight?: CSSProperties['fontWeight'];
   color?: CSSProperties['color'];
-  as?: ElementType;
 }
 
-const variantMapping: Record<FontVariant, string> = {
-  display: 'h1',
-  heading1: 'h1',
-  heading2: 'h2',
-  heading3: 'h3',
-  heading4: 'h4',
-  heading5: 'h5',
-  title: 'h5',
-  subTitle: 'h6',
-  body1: 'p',
-  body2: 'p',
-  caption: 'span',
-} as const;
+type Props<T extends ElementType> = PolymorphicProps<TypographyProps, T>;
 
-function Typography({ children, variant = 'body1', fontSize, fontWeight, color, as, ...props }: Props) {
-  const Component = as || variantMapping[variant] || 'span';
+function Typography<T extends ElementType = 'p'>({
+  as,
+  children,
+  variant = 'body1',
+  fontSize,
+  fontWeight,
+  color,
+  ...props
+}: Props<T>) {
+  const Component = as || variantMapping[variant] || 'p';
   const { fonts } = useTheme();
 
   return (
@@ -47,3 +42,17 @@ function Typography({ children, variant = 'body1', fontSize, fontWeight, color, 
 }
 
 export default Typography;
+
+const variantMapping: Record<FontVariant, string> = {
+  display: 'h1',
+  heading1: 'h1',
+  heading2: 'h2',
+  heading3: 'h3',
+  heading4: 'h4',
+  heading5: 'h5',
+  title: 'h5',
+  subTitle: 'h6',
+  body1: 'p',
+  body2: 'p',
+  caption: 'span',
+} as const;
