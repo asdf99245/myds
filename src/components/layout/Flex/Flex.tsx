1a0a0a1a3cd92f78fe54fab9689065e26d4e791d
Yunkyu Jung <39851220+asdf99245@users.noreply.github.com>
2023-05-25 14:34:39 +0900

    feat: Flex 다형성 컴포넌트로 변경

--- src/components/layout/Flex/Flex.tsx
+++ src/components/layout/Flex/Flex.tsx
@@ -1,34 +1,56 @@
-import { CSSProperties, PropsWithChildren } from 'react';
+import { CSSProperties, ElementType, ReactElement, forwardRef } from 'react';
+import { PolymorphicPropsWithRef, PolymorphicRef } from '@utils/type.util';
 
-type Props = PropsWithChildren<{
+interface FlexProps {
   inline?: boolean;
   flexDirection?: CSSProperties['flexDirection'];
   flexWrap?: CSSProperties['flexWrap'];
   alignItems?: CSSProperties['alignItems'];
   alignContent?: CSSProperties['alignContent'];
   justifyContent?: CSSProperties['justifyContent'];
-}>;
-
-function Flex({ children, ...props }: Props) {
-  return <div css={flexStyle(props)}>{children}</div>;
 }
+
+type Props<T extends ElementType> = PolymorphicPropsWithRef<FlexProps, T>;
+
+type FlexComponent = <T extends ElementType = DefaultElement>(props: { css: number } & Props<T>) => ReactElement | null;
+
+const DEFAULT_ELEMENT = 'div';
+type DefaultElement = typeof DEFAULT_ELEMENT;
+
+const Flex: FlexComponent = forwardRef(function Flex<T extends ElementType = DefaultElement>(
+  { as, children, ...props }: Props<T>,
+  ref: PolymorphicRef<T>,
+) {
+  const Component = as || DEFAULT_ELEMENT;
+
+  // TODO: Component에 emotion css를 전달할 방법을 고려해봐야 함. 타입을 어떻게 잡지?
+  return (
+    <Component ref={ref} {...props}>
+      {children}
+    </Component>
+  );
+});
+
+// function Flex({ children, ...props }: Props) {
+//   return <div css={flexStyle(props)}>{children}</div>;
+// }
 
 export default Flex;
 
-const flexStyle = ({
-  inline = false,
-  flexDirection,
-  flexWrap,
-  alignItems,
-  alignContent,
-  justifyContent,
-}: Omit<Props, 'children'>) => {
-  return {
-    display: inline ? 'inline-flex' : 'flex',
-    flexDirection,
-    flexWrap,
-    alignItems,
-    alignContent,
-    justifyContent,
-  };
-};
+// const flexStyle = ({
+//   inline = false,
+//   flexDirection,
+//   flexWrap,
+//   alignItems,
+//   alignContent,
+//   justifyContent,
+// }: Omit<Props, 'children'> = {}) => {
+//   return {
+//     display: inline ? 'inline-flex' : 'flex',
+//     flexDirection,
+//     flexWrap,
+//     alignItems,
+//     alignContent,
+//     justifyContent,
+//   };
+// };
