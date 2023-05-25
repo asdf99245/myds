b458415d4823baf02f6c635f64a88865a92b691d
Yunkyu Jung <39851220+asdf99245@users.noreply.github.com>
2023-05-25 13:01:59 +0900

    feat: children props 추가

--- src/components/layout/Flex/Flex.tsx
+++ src/components/layout/Flex/Flex.tsx
@@ -1,29 +1,28 @@
-import { CSSProperties } from 'react';
-import { Box } from '@components/layout/Box';
+import { CSSProperties, PropsWithChildren } from 'react';
 
-interface Props {
+type Props = PropsWithChildren<{
   inline?: boolean;
   flexDirection?: CSSProperties['flexDirection'];
   flexWrap?: CSSProperties['flexWrap'];
   alignItems?: CSSProperties['alignItems'];
   alignContent?: CSSProperties['alignContent'];
   justifyContent?: CSSProperties['justifyContent'];
-}
+}>;
 
-function Flex(props: Props) {
-  return <Box style={flexStyle(props)}>Flex</Box>;
+function Flex({ children, ...props }: Props) {
+  return <div css={flexStyle(props)}>{children}</div>;
 }
 
 export default Flex;
 
 const flexStyle = ({
   inline = false,
-  flexDirection = 'row',
-  flexWrap = 'nowrap',
+  flexDirection,
+  flexWrap,
   alignItems,
   alignContent,
   justifyContent,
-}: Props) => {
+}: Omit<Props, 'children'>) => {
   return {
     display: inline ? 'inline-flex' : 'flex',
     flexDirection,
