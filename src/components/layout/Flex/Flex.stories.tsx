708889a416c1e2a16b89d0e8e678222d93715d0f
Yunkyu Jung <39851220+asdf99245@users.noreply.github.com>
2023-05-25 14:35:22 +0900

    feat: Flex 스토리 생성

---
+++ src/components/layout/Flex/Flex.stories.tsx
@@ -0,0 +1,10 @@
+import type { Meta } from '@storybook/react';
+import Flex from './Flex';
+
+const meta: Meta<typeof Flex> = {
+  title: 'Components/Layout/Flex',
+  component: Flex,
+  argTypes: {},
+};
+
+export default meta;
